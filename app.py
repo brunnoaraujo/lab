115c1bb2940665f228cc6636625052b7e9c543ce
Brunno Araujo <brunnobaraujo@gmail.com>
2016-09-19 01:44:41 -0300

    Add config for heroku

--- app.py
+++ app.py
@@ -1,8 +1,4 @@
 from flask import Flask, render_template, redirect, url_for, request
-from flask_wtf import Form
-from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired
-
 import mysql.connector
 
 professor_name = None
