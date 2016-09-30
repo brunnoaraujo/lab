1b973684b730261599c6fceaffa0a024abb372d0
Brunno Araujo <brunnobaraujo@gmail.com>
2016-09-30 15:47:32 -0300

    Add autocomplete in professor and add iframe

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 from flask import Flask, render_template, redirect, url_for, request
 import mysql.connector
 
-professor_name = None
+professor_name = []
+professor = []
+disciplina = []
+turma = []
 
 app = Flask(__name__)
-#app.config['SECRET_KEY'] = 'very secret-y key value; shhhhh!'
-
 
 def db_connect():
     conn = mysql.connector.connect(user='root', password='root',
@@ -16,48 +17,56 @@ def db_connect():
 def index():
     return render_template('index.html')
 
-
-@app.route('/inicio')
-def inicio():
-    conn = db_connect()
-    cursor = conn.cursor()
-    cursor.execute("SELECT * FROM professor")
-    professor = cursor.fetchall()
-    conn.close()
-    return render_template('show-users.html', professor=professor)
-
-@app.route("/test" , methods=['GET', 'POST'])
-def test():
+@app.route("/qualquer" , methods=['GET', 'POST'])
+def qualquer():
+    global professor
+    global disciplina
+    global turma
     global professor_name
-    professor_name = request.form.get('comp_select')
-    print(professor_name)
+    url = ''
+    turmas = []
+    disciplinas = []
+    autocomplete = []
 
-    conn = db_connect()
-    cursor = conn.cursor()
-    cursor.execute("SELECT idprofessor FROM professor WHERE nome = '%s' " % professor_name)
-    idprofessor=cursor.fetchall()
-    print(idprofessor[0][0])
+    if request.form.get('professor') is None:
+        conn = db_connect()
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM professor")
+        professor = cursor.fetchall()
+        conn.close()
+        print(professor)
 
-    cursor.execute("SELECT * FROM disciplina WHERE disciplina = '%s' " % idprofessor[0][0])
-    disciplina = cursor.fetchall()
-    
-    return render_template('disciplina.html', disciplina=disciplina)
+        for prof in professor:
+            autocomplete.append(prof[1])
+        print(autocomplete)
 
-@app.route("/turma" , methods=['GET', 'POST'])
-def turma():
-    global professor_name
-    print(professor_name)
-    disciplina_name = request.form.get('comp_select')
-    
-    conn = db_connect()
-    cursor = conn.cursor()
-    cursor.execute("SELECT iddisciplina FROM disciplina WHERE nome_disciplina = '%s' " % disciplina_name)
-    idturma = cursor.fetchall()
-    print(idturma[0][0])
+    if request.form.get('professor'):
+        professor_name = request.form.get('professor')
+        print(professor_name)
+        conn = db_connect()
+        cursor = conn.cursor()
+        cursor.execute("SELECT idprofessor FROM professor WHERE nome = '%s' " % professor_name)
+        idprofessor=cursor.fetchall()
+        cursor.execute("SELECT * FROM disciplina WHERE disciplina = '%s' " % idprofessor[0][0])
+        disciplinas = cursor.fetchall()
 
-    cursor.execute("SELECT * FROM turma WHERE disciplina= '%s' " % idturma[0][0])
-    turma = cursor.fetchall()
-    return render_template('turma.html', turma=turma)
+    if request.form.get('disciplina') and request.form.get('disciplina') != 'None':
+        disciplina_name = request.form.get('disciplina')
+        conn = db_connect()
+        cursor = conn.cursor()
+        cursor.execute("SELECT iddisciplina FROM disciplina WHERE nome_disciplina = '%s' " % disciplina_name)
+        idturma = cursor.fetchall()
+        cursor.execute("SELECT * FROM turma WHERE disciplina= '%s' " % idturma[0][0])
+        turmas = cursor.fetchall()
+
+    if request.form.get('professor') and request.form.get('disciplina') and request.form.get('disciplina') != 'None'  and request.form.get('turma') and request.form.get('turma') != 'None':
+        turma = request.form.get('turma')
+        disciplina = request.form.get('disciplina')
+
+        url = 'http://fs24.formsite.com/UNIFACS/form1/fill?6=1'+"&9="+str(turma)+"&13="+str(disciplina)
+
+    return render_template('qualquer.html', professor=professor, disciplinas=disciplinas, turmas=turmas, url=url, autocomplete=autocomplete)
+
 
 
 if __name__ == '__main__':
