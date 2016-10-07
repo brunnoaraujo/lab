38a4be074c1e9faca1e931bff0a42d642a1aeb0b
Brunno Araujo <brunnobaraujo@gmail.com>
2016-10-06 23:26:07 -0300

    j

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, redirect, url_for, request
 import mysql.connector
+import json
 
 professor_name = []
 professor = []
@@ -22,6 +23,8 @@ def index():
     url = ''
     turmas = []
     disciplinas = []
+    disciplina = ''
+    turma = ''
     global autocomplete
     if request.form.get('professor') is None:
         conn = db_connect()
@@ -29,15 +32,12 @@ def index():
         cursor.execute("SELECT * FROM professor")
         professor = cursor.fetchall()
         conn.close()
-        print(professor)
         autocomplete =[]
         for prof in professor:
             autocomplete.append(prof[1])
-        print(autocomplete)
 
     if request.form.get('professor'):
         professor_name = request.form.get('professor')
-        print(professor_name)
         conn = db_connect()
         cursor = conn.cursor()
         cursor.execute("SELECT idprofessor FROM professor WHERE nome = '%s' " % professor_name)
@@ -45,22 +45,14 @@ def index():
         cursor.execute("SELECT * FROM disciplina WHERE disciplina = '%s' " % idprofessor[0][0])
         disciplinas = cursor.fetchall()
 
-    if request.form.get('disciplina') and request.form.get('disciplina') != 'None':
-        disciplina_name = request.form.get('disciplina')
-        conn = db_connect()
-        cursor = conn.cursor()
-        cursor.execute("SELECT iddisciplina FROM disciplina WHERE nome_disciplina = '%s' " % disciplina_name)
-        idturma = cursor.fetchall()
-        cursor.execute("SELECT * FROM turma WHERE disciplina= '%s' " % idturma[0][0])
-        turmas = cursor.fetchall()
+    if request.form.get('professor') and request.form.get('disciplina') and request.form.get('disciplina') != 'None':
+        nada = request.form.get('disciplina')
+        lista = nada.split('_', 1)
+        if(len(lista) > 1):
+            disciplina = lista[0]
+            turma = lista[1]
 
-    if request.form.get('professor') and request.form.get('disciplina') and request.form.get('disciplina') != 'None'  and request.form.get('turma') and request.form.get('turma') != 'None':
-        turma = request.form.get('turma')
-        disciplina = request.form.get('disciplina')
-
-        url = 'http://fs24.formsite.com/UNIFACS/form1/fill?6=1'+"&9="+str(turma)+"&13="+str(disciplina)
-
-    return render_template('professor.html', professor=professor, disciplinas=disciplinas, turmas=turmas, url=url, autocomplete=autocomplete)
+    return render_template('professor.html', professor=professor, disciplinas=disciplinas, turmas=turmas, autocomplete=autocomplete, disciplina=disciplina, turma=turma)
 
 
 if __name__ == '__main__':
