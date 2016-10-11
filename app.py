ee88ae32fc1ff072269d2f8830a79e00b1cbdfcf
Brunno Araujo <brunnobaraujo@gmail.com>
2016-10-10 21:19:06 -0300

    Add course name and shift

--- app.py
+++ app.py
@@ -16,16 +16,34 @@ def db_connect():
 
 @app.route("/" , methods=['GET', 'POST'])
 def index():
-    global professor
-    global disciplina
-    global turma
-    global professor_name
+    global professor, disciplina, turma, professor_name, autocomplete
     url = ''
     turmas = []
     disciplinas = []
     disciplina = ''
     turma = ''
-    global autocomplete
+    curso = ''
+    turno = ''
+    cursos = [{"acronym": "ADS", "name": "Analise e Desenvolvimento de Sistemas", "id": " "},
+        {"acronym": "AU", "name": "Arquitetura e Urbanismo", "id": "13"},
+        {"acronym": "CC", "name": "Ciencia da Computacao", "id": "1"},
+        {"acronym": "CO", "name": "Controle de Obras", "id": ""},
+        {"acronym": "EA", "name": "Engenharia Ambiental e Sanitaria", "id": "2"},
+        {"acronym": "EC", "name": "Engenharia Civil", "id": "3"},
+        {"acronym": "ES", "name": "Engenharia de Computacao", "id": "4"},
+        {"acronym": "EO", "name": "Engenharia de Petroleo", "id": "10"},
+        {"acronym": "EP", "name": "Engenharia de Producao", "id": "5"},
+        {"acronym": "EE", "name": "Engenharia Eletrica", "id": "6"},
+        {"acronym": "EM", "name": "Engenharia Mecanica", "id": "7"},
+        {"acronym": "ER", "name": "Engenharia Mecatronica", "id": "8"},
+        {"acronym": "EQ", "name": "Engenharia Quimica", "id": "11"},
+        {"acronym": "GA", "name": "Gestao Ambiental", "id": ""},
+        {"acronym": "PG", "name": "Petroleo e Gas", "id": ""},
+        {"acronym": "RC", "name": "Redes de Computadores", "id": ""},
+        {"acronym": "SI", "name": "Sistemas de Informacao", "id": "12"},
+        {"acronym": "PG", "name": "Petroleo e Gas", "id": ""}, 
+        ]
+
     if request.form.get('professor') is None:
         conn = db_connect()
         cursor = conn.cursor()
@@ -51,8 +69,19 @@ def index():
         if(len(lista) > 1):
             disciplina = lista[0]
             turma = lista[1]
+            nada = turma.split('-', 2)
+            acronym = nada[1]
+            turno = nada[2][0]
+            if(turno=='M'):
+                turno=1
+            if(turno=='V'):
+                turno=2
+            if(turno=='N'):
+                turno=3
+            curso = filter(lambda curso:curso['acronym']==acronym,cursos)
+            curso = curso[0]['id']
 
-    return render_template('professor.html', professor=professor, disciplinas=disciplinas, turmas=turmas, autocomplete=autocomplete, disciplina=disciplina, turma=turma, professor_name=professor_name)
+    return render_template('professor.html', professor=professor, disciplinas=disciplinas, turmas=turmas, autocomplete=autocomplete, disciplina=disciplina, turma=turma, professor_name=professor_name, turno=turno, curso=curso)
 
 
 if __name__ == '__main__':
